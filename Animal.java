8c0bee4aa17d812c70b67c1511ecbeb440d4feb1
Farhad Reza <92999132+farhad-2021@users.noreply.github.com>
2023-10-11 22:55:47 +0600

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,16 @@
+
+public class Animal {
+    
+    void display(){
+    System.out.println("All animal is here");
+    }
+}
+class Dog extends Animal{
+    
+    @Override
+    void display(){
+     System.out.println("The dog is barking");
+       }
+   }
+   
+
---
+++ Animali.java
@@ -0,0 +1,13 @@
+
+interface  Animali {
+   
+     void display(){
+    System.out.println("Animal class");
+    }
+}
+class dog implements Animali{
+     System.out.println("Dog class");
+
+}
+
+  
